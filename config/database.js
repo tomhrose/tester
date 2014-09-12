475f5713fc9dfb0d2a0dfadab80f741238a70443
Tom Rose <tomrose@Toms-MacBook-Pro.local>
2014-09-11 23:28:56 -0400

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,10 @@
+/**
+ * Created by tomrose on 9/11/14.
+ */
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://tomrose:underdog@kahana.mongohq.com:10023/app29485782' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
+
