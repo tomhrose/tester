26ef644a3f4aa3927a4307ee1982708632cda6e1
tomrose <tomhrose@gmail.com>
2014-09-12 16:02:16 -0400

    Updated for open activities

--- app/routes.js
+++ app/routes.js
@@ -61,6 +61,17 @@ module.exports = function(app, passport) {
     });
 
     // =====================================
+    // OPENACTIVITIES SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/openactivities', isLoggedIn, function(req, res) {
+        res.render('openactivities.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
     // LOGOUT ==============================
     // =====================================
     app.get('/logout', function(req, res) {
